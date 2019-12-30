cbfe21816b9ea367a4cf52fed179cfb80ff629cd
Vince Broz <vince@broz.cc>
2019-12-30 12:09:56 -0500

    Factor out MessageCreator class

--- lib/pronto/bigfiles/patch_inspector.rb
+++ lib/pronto/bigfiles/patch_inspector.rb
@@ -5,16 +5,30 @@ require 'pronto'
 module Pronto
   # Performs incremental quality reporting for the bigfiles gem
   class BigFiles < Runner
-    class PatchInspector
-      def initialize(bigfiles_result)
+    class MessageCreator
+      attr_reader :patch
+
+      def initialize(patch)
+        @patch = patch
       end
 
-      def inspect_patch(patch)
+      def create_message
         path = patch.delta.new_file[:path]
         line = patch.added_lines.first
         level = :warning
         msg = ""
         Message.new(path, line, level, msg)
+      end
+    end
+
+    class PatchInspector
+      def initialize(bigfiles_result,
+                     message_creator_class: MessageCreator)
+        @message_creator_class = message_creator_class
+      end
+
+      def inspect_patch(patch)
+        @message_creator_class.new(patch).create_message
       end
     end
   end
