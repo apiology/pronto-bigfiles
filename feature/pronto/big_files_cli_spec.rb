a92ffb11c98190d6e0185ce7964a6632294ba30b
Vince Broz <vince@broz.cc>
2019-12-30 15:25:35 -0500

    Add more feature specs

--- feature/pronto/big_files_cli_spec.rb
+++ feature/pronto/big_files_cli_spec.rb
@@ -66,13 +66,14 @@ describe Pronto::BigFiles do
     # and the total ends up above 300
     #
     # ...and we only complain once per file
-    context 'when single file added to ' \
+    context 'when single file net added to ' \
             'that is one of the three complained about, ' \
             'and is above limit' do
       let(:expected_output) do
-        'one_line_added_above_limit.rb:302 W: This file, one of the 3 largest in the project, increased in size to 301 lines'
+        "one_line_added_above_limit.rb:302 W: This file, one of the 3 largest in the project, increased in size to 301 lines.  Is it complex enough to refactor?\n"
       end
 
+      # TODO: Replicate net added to aspect
       let(:example_files_committed) do
         {
           'one_line_added_above_limit.rb' => ("\n" * 301),
@@ -86,7 +87,7 @@ describe Pronto::BigFiles do
       end
 
       it 'complains on line of first change' do
-        expect(out).to include(expected_output)
+        expect(out).to eq(expected_output)
         expect(exit_code).to eq(0)
       end
     end
@@ -94,13 +95,54 @@ describe Pronto::BigFiles do
     context 'when single file removed from ' \
             'that is one of the three complained about, ' \
             'and is above limit' do
-      xit 'does not complain'
+      let(:expected_output) do
+        ''
+      end
+
+      let(:example_files_committed) do
+        {
+          'one_line_removed_above_limit.rb' => ("\n" * 302),
+        }
+      end
+
+      let(:example_files_staged) do
+        {
+          'one_line_removed_above_limit.rb' => ("\n" * 301),
+        }
+      end
+
+      it 'does not complain' do
+        expect(out).to eq(expected_output)
+        expect(exit_code).to eq(0)
+      end
     end
 
-    context 'when single file added to ' \
+    context 'when single file net added to ' \
             'that is not one of the three complained about, ' \
             'and is above limit' do
-      xit 'does not complain'
+      let(:expected_output) do
+        ''
+      end
+
+      let(:example_files_committed) do
+        {
+          'existing_large_file_1.rb' => ("\n" * 100),
+          'existing_large_file_2.rb' => ("\n" * 100),
+          'existing_large_file_3.rb' => ("\n" * 100),
+          'small_large_file_one_line_added.rb' => ("\n" * 50),
+        }
+      end
+
+      let(:example_files_staged) do
+        {
+          'small_large_file_one_line_added.rb' => ("\n" * 51),
+        }
+      end
+
+      it 'does not complain' do
+        expect(out).to eq(expected_output)
+        expect(exit_code).to eq(0)
+      end
     end
 
     context 'when single file untouched ' \
@@ -109,7 +151,7 @@ describe Pronto::BigFiles do
       xit 'does not complain'
     end
 
-    context 'when single file added to ' \
+    context 'when single file net added to ' \
             'that is one of the three complained about, ' \
             'but is below limit of 300' do
       xit 'does not complain'
